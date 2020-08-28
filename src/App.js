b08f1bd039216410461e2b84bced5d31f33bb301
xyrmovich <danikx511@gmail.com>
2020-08-28 19:58:14 +0300

    Add routing to MenuItem with HOC

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact={true} path='/' component={HomePage} />
     </div>
   );
 }
